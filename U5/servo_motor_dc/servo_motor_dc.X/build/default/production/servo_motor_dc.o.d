3a0cfa22473d82248d5f4a60a79275febe091129
OscarFriasZavalza <friaszavalzao@gmail.com>
2023-05-15 19:31:19 -0700

    version con servo y motor dc

---
+++ U5/servo_motor_dc/servo_motor_dc.X/build/default/production/servo_motor_dc.o.d
@@ -0,0 +1,3 @@
+build/default/production/servo_motor_dc.o:  \
+	C:/SP_PROGRAMAS/U5/servo_motor_dc/servo_motor_dc.X/servo_motor_dc.asm  \
+
---
+++ U5/servomotor_diferentes_pines/servo_pines_dif.X/build/default/production/servomotor_diferentes_pines.o.d
@@ -0,0 +1,3 @@
+build/default/production/servomotor_diferentes_pines.o:  \
+	C:/SP_PROGRAMAS/U5/servomotor_diferentes_pines/servo_pines_dif.X/servomotor_diferentes_pines.asm  \
+
